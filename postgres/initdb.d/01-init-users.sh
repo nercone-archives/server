9c93aca3360aac7881ff4231f03739a241dc5665
nercone-dev <nercone@nercone.dev>
2026-08-24 12:40:49 +0900

    --

--- postgres/initdb.d/01-init-users.sh
+++ postgres/initdb.d/01-init-users.sh
@@ -4,7 +4,4 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER gitea WITH PASSWORD '${GITEA_DB_PASSWORD}';
     CREATE DATABASE gitea OWNER gitea;
-
-    CREATE USER keycloak WITH PASSWORD '${KEYCLOAK_DB_PASSWORD}';
-    CREATE DATABASE keycloak OWNER keycloak;
 EOSQL
